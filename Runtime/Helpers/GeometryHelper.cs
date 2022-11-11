9ac0a773bd8f5a283d751b8a31ea5c5258e46c52
sponticelli <sandro.ponticelli@gmail.com>
2022-11-11 15:27:31 +0100

    feat: add methods to find distance of point from a line and the projected point on a line

--- Runtime/Helpers/GeometryHelper.cs
+++ Runtime/Helpers/GeometryHelper.cs
@@ -28,5 +28,146 @@ namespace LiteNinja.Common
     {
       return Vector3.Dot(point - halfPlanePoint, halfPlaneDirection) >= 0;
     }
+
+    /// <summary>
+    /// Find the projection of a point onto a line.
+    /// </summary>
+    /// <param name="point">The point to project.</param>
+    /// <param name="linePoint">A point on the line.</param>
+    /// <param name="lineDirection">The direction of the line.</param>
+    /// <returns>The projection of the point onto the line.</returns>
+    public static Vector2 ProjectPointOnLine(Vector2 point, Vector2 linePoint, Vector2 lineDirection)
+    {
+      return linePoint + Vector2.Dot(point - linePoint, lineDirection) * lineDirection;
+    }
+
+    /// <summary>
+    /// Find the projection of a point onto a line. The line is defined by two points.
+    /// </summary>
+    /// <param name="point">The point to project</param>
+    /// <param name="firstPoint">first point of the line</param>
+    /// <param name="seconPoint">second point of the line</param>
+    /// <returns>The projection of the point onto the line defined by the 2 points</returns>
+    public static Vector2 ProjectPointOnLine2(Vector2 point, Vector2 firstPoint, Vector2 secondPoint)
+    {
+      return ProjectPointOnLine(point, firstPoint, secondPoint - firstPoint);
+    }
+    
+    /// <summary>
+    /// Find the projection of a point onto a line segment.
+    /// </summary>
+    /// <param name="point">The point to project.</param>
+    /// <param name="linePoint1">A point on the line segment.</param>
+    /// <param name="linePoint2">Another point on the line segment.</param>
+    /// <returns>The projection of the point onto the line segment.</returns>
+    public static Vector2 ProjectPointOnLineSegment(Vector2 point, Vector2 linePoint1, Vector2 linePoint2)
+    {
+      var lineDirection = linePoint2 - linePoint1;
+      var projectedPoint = ProjectPointOnLine(point, linePoint1, lineDirection);
+      var projectedPointDistance = Vector2.Distance(linePoint1, projectedPoint);
+      var lineDistance = Vector2.Distance(linePoint1, linePoint2);
+      if (projectedPointDistance > lineDistance)
+      {
+        return linePoint2;
+      }
+
+      return projectedPointDistance < 0 ? linePoint1 : projectedPoint;
+    }
+    
+    
+    /// <summary>
+    /// Find the distance between a point and a line.
+    /// </summary>
+    /// <param name="point">The point.</param>
+    /// <param name="linePoint">A point on the line.</param>
+    /// <param name="lineDirection">The direction of the line.</param>
+    /// <returns>The distance between the point and the line.</returns>
+    public static float DistancePointLine(Vector2 point, Vector2 linePoint, Vector2 lineDirection)
+    {
+      return Vector2.Distance(point, ProjectPointOnLine(point, linePoint, lineDirection));
+    }
+    
+    /// <summary>
+    /// Find the distance between a point and a line. The line is defined by two points.
+    /// </summary>
+    /// <param name="point">The point.</param>
+    /// <param name="firstPoint">first point of the line</param>
+    /// <param name="seconPoint">second point of the line</param>
+    /// <returns>The distance between the point and the line.</returns>
+    public static float DistancePointLine2(Vector2 point, Vector2 firstPoint, Vector2 secondPoint)
+    {
+      return DistancePointLine(point, firstPoint, secondPoint - firstPoint);
+    }
+
+    /// <summary>
+    /// Find the projection of a point onto a line.
+    /// </summary>
+    /// <param name="point">The point to project.</param>
+    /// <param name="linePoint">A point on the line.</param>
+    /// <param name="lineDirection">The direction of the line.</param>
+    /// <returns>The projection of the point onto the line.</returns>
+    public static Vector3 ProjectPointOnLine(Vector3 point, Vector3 linePoint, Vector3 lineDirection)
+    {
+      return linePoint + Vector3.Dot(point - linePoint, lineDirection) * lineDirection;
+    }
+
+    /// <summary>
+    /// Find the projection of a point onto a line. The line is defined by two points.
+    /// </summary>
+    /// <param name="point">The point to project</param>
+    /// <param name="firstPoint">first point of the line</param>
+    /// <param name="seconPoint">second point of the line</param>
+    /// <returns>The projection of the point onto the line defined by the 2 points</returns>
+    public static Vector3 ProjectPointOnLine2(Vector3 point, Vector3 firstPoint, Vector3 secondPoint)
+    {
+      return ProjectPointOnLine(point, firstPoint, secondPoint - firstPoint);
+    }
+    
+    /// <summary>
+    /// Find the projection of a point onto a line segment.
+    /// </summary>
+    /// <param name="point">The point to project.</param>
+    /// <param name="linePoint1">A point on the line segment.</param>
+    /// <param name="linePoint2">Another point on the line segment.</param>
+    /// <returns>The projection of the point onto the line segment.</returns>
+    public static Vector3 ProjectPointOnLineSegment(Vector3 point, Vector3 linePoint1, Vector3 linePoint2)
+    {
+      var lineDirection = linePoint2 - linePoint1;
+      var projectedPoint = ProjectPointOnLine(point, linePoint1, lineDirection);
+      var projectedPointDistance = Vector3.Distance(linePoint1, projectedPoint);
+      var lineDistance = Vector3.Distance(linePoint1, linePoint2);
+      if (projectedPointDistance > lineDistance)
+      {
+        return linePoint2;
+      }
+
+      return projectedPointDistance < 0 ? linePoint1 : projectedPoint;
+    }
+    
+    
+    /// <summary>
+    /// Find the distance between a point and a line.
+    /// </summary>
+    /// <param name="point">The point.</param>
+    /// <param name="linePoint">A point on the line.</param>
+    /// <param name="lineDirection">The direction of the line.</param>
+    /// <returns>The distance between the point and the line.</returns>
+    public static float DistancePointLine(Vector3 point, Vector3 linePoint, Vector3 lineDirection)
+    {
+      return Vector3.Distance(point, ProjectPointOnLine(point, linePoint, lineDirection));
+    }
+    
+    /// <summary>
+    /// Find the distance between a point and a line. The line is defined by two points.
+    /// </summary>
+    /// <param name="point">The point.</param>
+    /// <param name="firstPoint">first point of the line</param>
+    /// <param name="seconPoint">second point of the line</param>
+    /// <returns>The distance between the point and the line.</returns>
+    public static float DistancePointLine2(Vector3 point, Vector3 firstPoint, Vector3 secondPoint)
+    {
+      return DistancePointLine(point, firstPoint, secondPoint - firstPoint);
+    }
+    
   }
 }
